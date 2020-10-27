e0034dce30179b1896f82c44afbacf8e3f55dfe1
Bohdan-pidkovych <52783471+Bohdan-pidkovych@users.noreply.github.com>
2020-10-27 22:47:00 +0200

    Create filtration wizards

---
+++ js/render.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
+  var userDialog = document.querySelector('.setup');
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+  var renderWizards = function (wizards) {
+    var fragment = document.createDocumentFragment();
+    var takeNumber = wizards.length > MAX_SIMILAR_WIZARD_COUNT
+      ? MAX_SIMILAR_WIZARD_COUNT
+      : wizards.length;
+
+    similarListElement.innerHTML = '';
+
+    for (var i = 0; i < takeNumber; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  window.render = {
+    renderWizards
+  };
+})();
